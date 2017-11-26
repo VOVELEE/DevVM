c67c5bec8c1ef6cc7edbfb303c5a73c3822a1ae6
Georgi Slavov <georgi.slavov@vovelee.net>
2017-11-26 14:14:45 +0200

    Added PowerPlan and ServerManger

---
+++ DSCResources/DevVMAzureBuild.ps1
@@ -0,0 +1,9 @@
+$CompilationJob = Start-AzureRmAutomationDscCompilationJob -ResourceGroupName "MyResourceGroup" -AutomationAccountName "MyAutomationAccount" -ConfigurationName "SampleConfig"
+
+while($CompilationJob.EndTime –eq $null -and $CompilationJob.Exception –eq $null)
+{
+    $CompilationJob = $CompilationJob | Get-AzureRmAutomationDscCompilationJob
+    Start-Sleep -Seconds 3
+}
+
+$CompilationJob | Get-AzureRmAutomationDscCompilationJobOutput –Stream Any
--- DSCResources/DevVMConfig.ps1
+++ DSCResources/DevVMConfig.ps1
@@ -1,46 +1,78 @@
 Configuration DevelopmentVM
 {
-        Import-DscResource -ModuleName xPSDesiredStateConfiguration
-        Import-DscResource –ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+    Import-DscResource -ModuleName xComputerManagement
+    Import-DscResource –ModuleName PSDesiredStateConfiguration
 
-        $VSCodePackageLocalPath = "c:\temp\vscodeSetup.exe"
-        $GoogleChromePackageLocalPath = "c:\temp\ChromeStandaloneSetup.msi"
+    $VSCodePackageLocalPath = "c:\temp\vscodeSetup.exe"
+    $GoogleChromePackageLocalPath = "c:\temp\ChromeStandaloneSetup.msi"
 
-        Node localhost {
-            #Download a package
-            xRemoteFile VSCodePackage {
-                Uri = 'https://go.microsoft.com/fwlink/?Linkid=852157'
-                DestinationPath = $VSCodePackageLocalPath
-            }
-
-            xRemoteFile GoogleChromePackage {
-                Uri = "https://dl.google.com/tag/s/appguid={8A69D345-D564-463C-AFF1-A69D9E530F96}&iid={00000000-0000-0000-0000-000000000000}&lang=en&browser=3&usagestats=0&appname=Google%2520Chrome&needsadmin=prefers/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi"
-                DestinationPath = $GoogleChromePackageLocalPath
-            }
-
-            <#Install Application
-            Package VsCode {
-                Ensure = "Present"
-                Path = $VSCodePackageLocalPath
-                Name = "Microsoft Monitoring Agent"
-                ProductId = ""
-                Arguments = '/C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=' + $OmsWorkspaceID + ' OPINSIGHTS_WORKSPACE_KEY=' + $OmsWorkspaceKey + ' AcceptEndUserLicenseAgreement=1"'
-                DependsOn = "[xRemoteFile]VSCodePackage"
-            }
-            #>
-
-             Package GoogleChrome  {
-                Ensure = 'Present'
-                Name = 'Google Chrome'
-                Path = $GoogleChromePackageLocalPath
-                ProductId = ''
-                Arguments = '/silent /install'
-                DependsOn = "[xRemoteFile]GoogleChromePackage"
-             }
+    Node localhost {
+        #Configurations
+        xPowerPlan SetPlanHighPerformance
+        {
+            IsSingleInstance = 'Yes'
+            Name             = 'High performance'
         }
 
+        Script DisableServerManager {
+            GetScript = {
+                Return @{
+                    Result = [string]$(Get-ScheduledTask -TaskName ServerManager | Select TaskName, State)
+                }
+            }
+
+            # Must return a boolean: $true or $false
+            TestScript = {
+                If ((Get-ScheduledTask -TaskName ServerManager).State -like "Disabled")
+                {
+                    Write-Verbose "Server Manager is disabled"
+                    Return $true
+                }
+                Else
+                {
+                    Write-Verbose "Server Manager is enabled"
+                    Return $false
+                }
+            }
+
+            # Returns nothing
+            SetScript = {
+                Write-Verbose "Stopping ServerManager scheduled task"
+                Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
+            }
+        }
+
+        #Download a package
+        xRemoteFile VSCodePackage {
+            Uri = 'https://go.microsoft.com/fwlink/?Linkid=852157'
+            DestinationPath = $VSCodePackageLocalPath
+        }
+
+        xRemoteFile GoogleChromePackage {
+            Uri = "https://dl.google.com/tag/s/appguid={8A69D345-D564-463C-AFF1-A69D9E530F96}&iid={00000000-0000-0000-0000-000000000000}&lang=en&browser=3&usagestats=0&appname=Google%2520Chrome&needsadmin=prefers/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi"
+            DestinationPath = $GoogleChromePackageLocalPath
+        }
+
+        <#Install Application
+        Package VsCode {
+            Ensure = "Present"
+            Path = $VSCodePackageLocalPath
+            Name = "Microsoft Monitoring Agent"
+            ProductId = ""
+            Arguments = '/C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=' + $OmsWorkspaceID + ' OPINSIGHTS_WORKSPACE_KEY=' + $OmsWorkspaceKey + ' AcceptEndUserLicenseAgreement=1"'
+            DependsOn = "[xRemoteFile]VSCodePackage"
+        }
+        #>
+
+        Package GoogleChrome  {
+            Ensure = 'Present'
+            Name = 'Google Chrome'
+            Path = $GoogleChromePackageLocalPath
+            ProductId = ''
+            Arguments = '/silent /install'
+            DependsOn = "[xRemoteFile]GoogleChromePackage"
+        }
     }
 
-DevelopmentVM -OutputPath 'Test'
-
-Start-DscConfiguration -ComputerName localhost -Path 'Test' -Wait -Verbose -Force
+}
